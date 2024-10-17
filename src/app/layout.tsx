c062d52bbb0b83ffe517eac8e24c1cae14279250
Pasindu Pramodya <pasindudixon25@gmail.com>
2024-10-17 16:16:16 +0530

    add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Script from "next/script";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="icon.svg" type="image/svg+xml" sizes="any" />
-      <meta name="keywords" content="developer, pasindu pramodya,pasindu p konghawaththa,programmer, software engineer , software developer, web developer ,full stack developer, front end developer, website developer, github ,backend developer, java developer, portfolio,codeberg,littlelink"></meta>
+      <meta
+        name="keywords"
+        content="developer, pasindu pramodya,pasindu p konghawaththa,programmer, software engineer , software developer, web developer ,full stack developer, front end developer, website developer, github ,backend developer, java developer, portfolio,codeberg,littlelink"
+      ></meta>
       <meta name="language" content="English"></meta>
       <meta name="author" content="pasindu pramodya"></meta>
       <body
@@ -34,6 +37,16 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Script id="microsoft-clarity-analytics">
+        {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "oe053zd2hq");
+
+  `}
+      </Script>
     </html>
   );
 }
