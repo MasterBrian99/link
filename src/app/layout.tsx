50af304aa0a15092179998f4af019b2c1bb06f69
Pasindu Pramodya <pasindudixon25@gmail.com>
2024-10-17 16:24:13 +0530

    remove clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Script from "next/script";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -37,16 +36,6 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <Script id="microsoft-clarity-analytics">
-        {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "oe053zd2hq");
-
-  `}
-      </Script>
     </html>
   );
 }
